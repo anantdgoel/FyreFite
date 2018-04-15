bb05833c9a61e5e078b73436fb842cbcc2c272be
Anant Goel <anantdgoel@gmail.com>
2018-04-15 02:30:08 -0400

    adding game room

--- frontend/app/src/main/java/edu/dartmouth/cs/battleroyalego/GameRoom.java
+++ frontend/app/src/main/java/edu/dartmouth/cs/battleroyalego/GameRoom.java
@@ -2,14 +2,18 @@ package edu.dartmouth.cs.battleroyalego;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GameRoom extends AppCompatActivity {
+
+    public String gameName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_room);
-
-
+        gameName = getIntent().getStringExtra("GAME_NAME");
+        final TextView gameNameText = findViewById(R.id.gameName);
+        gameNameText.setText(gameName);
     }
 }
