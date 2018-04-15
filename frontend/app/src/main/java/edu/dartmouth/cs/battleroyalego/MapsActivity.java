c2327435fb29e654a84b72e20356bfc71152b962
Eric Zhang <ezzhang@Erics-MacBook-Air.local>
2018-04-15 07:47:59 -0400

    game logic

--- frontend/app/src/main/java/edu/dartmouth/cs/battleroyalego/MapsActivity.java
+++ frontend/app/src/main/java/edu/dartmouth/cs/battleroyalego/MapsActivity.java
@@ -3,6 +3,7 @@ package edu.dartmouth.cs.battleroyalego;
 import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
@@ -56,6 +57,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        //check
+        checkVictoryAndDeath();
 
         if(justStart){
             healthPoints = 100;
@@ -118,6 +122,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 healthPoints = 100;
             }
             return true;
+        }
+    }
+
+    public void checkVictoryAndDeath(){
+        if(nPlayers <= 1){
+            Intent intent = new Intent(MapsActivity.this, VictoryActivity.class);
+            startActivity(intent);
+        }else if(healthPoints <= 0){
+            Intent intent = new Intent(MapsActivity.this, DeathActivity.class);
+            startActivity(intent);
         }
     }
 
