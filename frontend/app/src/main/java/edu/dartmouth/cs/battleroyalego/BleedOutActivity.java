3b702599cbdaa889a4320a56fabef30613ae8ad8
yiphei <yanyiphei@gmail.com>
2018-04-15 02:29:22 -0400

    animation

--- frontend/app/src/main/java/edu/dartmouth/cs/battleroyalego/BleedOutActivity.java
+++ frontend/app/src/main/java/edu/dartmouth/cs/battleroyalego/BleedOutActivity.java
@@ -4,16 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class BleedOutActivity extends AppCompatActivity {
 
     Button mFirstAid, mDeath;
 
+    ImageView v;
+    Animation animation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bleed_out);
+
+
+        v = (ImageView) findViewById(R.id.blood_image);
+        animation = AnimationUtils.loadAnimation(this,R.anim.animation);
+        v.setAnimation(animation);
+
 
         mFirstAid = findViewById(R.id.button_aid);
         mDeath = findViewById(R.id.button_give);
--- frontend/app/src/main/java/edu/dartmouth/cs/battleroyalego/VictoryActivity.java
+++ frontend/app/src/main/java/edu/dartmouth/cs/battleroyalego/VictoryActivity.java
@@ -3,13 +3,17 @@ package edu.dartmouth.cs.battleroyalego;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
 import android.opengl.EGLConfig;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import javax.microedition.khronos.opengles.GL10;
 
@@ -17,12 +21,18 @@ public class VictoryActivity extends AppCompatActivity {
 
     Button mMenu;
 
+    ImageView v;
+    Animation animation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_victory);
 
         mMenu = findViewById(R.id.button_main);
+        v = (ImageView) findViewById(R.id.victory_image);
+        animation = AnimationUtils.loadAnimation(this,R.anim.animation);
+        v.setAnimation(animation);
 
         mMenu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,60 +43,7 @@ public class VictoryActivity extends AppCompatActivity {
         });
     }
 
-    public class MyGLRenderer implements GLSurfaceView.Renderer {
 
-        public void onSurfaceCreated(GL10 unused, EGLConfig config) {
-            // Set the background frame color
-            GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        }
-
-        public void onDrawFrame(GL10 unused) {
-            // Redraw background color
-            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        }
-
-        @Override
-        public void onSurfaceCreated(GL10 gl, javax.microedition.khronos.egl.EGLConfig config) {
-
-        }
-
-        public void onSurfaceChanged(GL10 unused, int width, int height) {
-            GLES20.glViewport(0, 0, width, height);
-        }
-    }
-
-    public class OpenGLES20Activity extends Activity {
-
-        private GLSurfaceView mGLView;
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-
-            // Create a GLSurfaceView instance and set it
-            // as the ContentView for this Activity.
-            mGLView = new MyGLSurfaceView(this);
-            setContentView(mGLView);
-        }
-    }
-
-
-    class MyGLSurfaceView extends GLSurfaceView {
-
-        private final MyGLRenderer mRenderer;
-
-        public MyGLSurfaceView(Context context){
-            super(context);
-
-            // Create an OpenGL ES 2.0 context
-            setEGLContextClientVersion(2);
-
-            mRenderer = new MyGLRenderer();
-
-            // Set the Renderer for drawing on the GLSurfaceView
-            setRenderer(mRenderer);
-        }
-    }
 
 
 
