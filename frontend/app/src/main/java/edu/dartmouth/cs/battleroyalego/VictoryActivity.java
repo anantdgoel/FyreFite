b5154ffa1b6bfcae36827ebab298baee826022ce
yiphei <yanyiphei@gmail.com>
2018-04-15 01:18:35 -0400

    merge

--- frontend/app/src/main/java/edu/dartmouth/cs/battleroyalego/VictoryActivity.java
+++ frontend/app/src/main/java/edu/dartmouth/cs/battleroyalego/VictoryActivity.java
@@ -1,10 +1,17 @@
 package edu.dartmouth.cs.battleroyalego;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.opengl.EGLConfig;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import javax.microedition.khronos.opengles.GL10;
 
 public class VictoryActivity extends AppCompatActivity {
 
@@ -25,6 +32,64 @@ public class VictoryActivity extends AppCompatActivity {
             }
         });
     }
+
+    public class MyGLRenderer implements GLSurfaceView.Renderer {
+
+        public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+            // Set the background frame color
+            GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        }
+
+        public void onDrawFrame(GL10 unused) {
+            // Redraw background color
+            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        }
+
+        @Override
+        public void onSurfaceCreated(GL10 gl, javax.microedition.khronos.egl.EGLConfig config) {
+
+        }
+
+        public void onSurfaceChanged(GL10 unused, int width, int height) {
+            GLES20.glViewport(0, 0, width, height);
+        }
+    }
+
+    public class OpenGLES20Activity extends Activity {
+
+        private GLSurfaceView mGLView;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Create a GLSurfaceView instance and set it
+            // as the ContentView for this Activity.
+            mGLView = new MyGLSurfaceView(this);
+            setContentView(mGLView);
+        }
+    }
+
+
+    class MyGLSurfaceView extends GLSurfaceView {
+
+        private final MyGLRenderer mRenderer;
+
+        public MyGLSurfaceView(Context context){
+            super(context);
+
+            // Create an OpenGL ES 2.0 context
+            setEGLContextClientVersion(2);
+
+            mRenderer = new MyGLRenderer();
+
+            // Set the Renderer for drawing on the GLSurfaceView
+            setRenderer(mRenderer);
+        }
+    }
+
+
+
 
     @Override
     public void onBackPressed() {
