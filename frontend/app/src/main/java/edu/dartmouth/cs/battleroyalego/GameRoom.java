bda70dc1740696cf087cec59ff4c49c42e4a21d3
Junhwi Kim <junhwi.kim.20@dartmouth.edu>
2018-04-14 21:55:14 -0400

    pass location

--- frontend/app/src/main/java/edu/dartmouth/cs/battleroyalego/GameRoom.java
+++ frontend/app/src/main/java/edu/dartmouth/cs/battleroyalego/GameRoom.java
@@ -1,19 +1,14 @@
 package edu.dartmouth.cs.battleroyalego;
 
-import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class JoinActivity extends AppCompatActivity {
-
-    Location user_location;
+public class GameRoom extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_join);
-
-        user_location = getIntent().getParcelableExtra("USER_LOCATION");
+        setContentView(R.layout.activity_game_room);
 
 
     }
--- frontend/app/src/main/java/edu/dartmouth/cs/battleroyalego/MakeActivity.java
+++ frontend/app/src/main/java/edu/dartmouth/cs/battleroyalego/MakeActivity.java
@@ -1,13 +1,35 @@
 package edu.dartmouth.cs.battleroyalego;
 
+import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.autofill.AutofillValue;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MakeActivity extends AppCompatActivity {
+
+    Location user_location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make);
+
+        Button startGame = findViewById(R.id.create_game);
+        user_location = getIntent().getParcelableExtra("USER_LOCATION");
+
+        startGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MakeActivity.this, GameRoom.class);
+                intent.putExtra("USER_LOCATION", user_location);
+                intent.putExtra("GAME_LOCATION", user_location);
+                startActivity(intent);
+            }
+        });
     }
+
 }
